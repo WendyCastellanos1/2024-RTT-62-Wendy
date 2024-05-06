2cb48a63da0c75a011673171a4ad2ab0f9b9c137
WendyCastellanos1 <168469989+WendyCastellanos1@users.noreply.github.com>
2024-05-05 20:17:31 -0500

    Improved JavaStringTokens (from HackerRank) but regex expression (from HR) produces tokens of empty strings. will revisit....

--- java-classwork/JavaClasswork/src/main/java/org/example/PA_303_3_1/JavaStringTokens.java
+++ java-classwork/JavaClasswork/src/main/java/org/example/PA_303_3_1/JavaStringTokens.java
@@ -1,10 +1,41 @@
 package org.example.PA_303_3_1;
 
-public class JavaStringTokens {
+import java.io.*;
+import java.util.*;
+import java.lang.String;
+import java.util.Scanner;
+
+//lesson: split function uses regex to split into tokens stored in a String array
+//String[] tokens = S.split("[^a-zA-Z]");
+
+public class JavaStringTokens {     // public class Solution {
 
     public static void main(String[] args) {
 
-        
-    }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a single string, like: He is a very very good boy, isn't he?  ");
+        String s = scan.nextLine();
 
+        //String regex = "[A-Za-z !,?._'@]+";      // or ?    [A-Za-z !,?._'@]+,  comma or no?
+
+        //split string into tokens
+       String[] tokenArray = s.split( "[A-Za-z!,?._'@]+"); //doesn't work
+        //String[] tokenArray = s.split( "\s");
+
+        int numTokens = 0;
+
+        for (int i = 0; i < tokenArray.length; ++i){
+            if (!tokenArray[i].isEmpty()) {
+                numTokens++;
+            }
+        }
+        System.out.println(numTokens);
+
+        for (int i = 0; i < tokenArray.length; ++i){
+            if (!tokenArray[i].isEmpty()){
+                System.out.println(tokenArray[i]);
+            }
+        }
+        scan.close();
+    }
 }
--- java-classwork/JavaClasswork/src/main/java/org/example/PA_303_3_1/PracticeAssignmentStrings.java
+++ java-classwork/JavaClasswork/src/main/java/org/example/PA_303_3_1/PracticeAssignmentStrings.java
@@ -10,4 +10,5 @@ public class PracticeAssignmentStrings {
     Screenshots placed in reach of GitHub.
 
      */
+}
 
