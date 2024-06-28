e6e77ea3025f39ee9a038c891d912f934e278f2c
WendyCastellanos1 <168469989+WendyCastellanos1@users.noreply.github.com>
2024-06-27 21:43:50 -0600

    added HTML5 regex for email fields, first name, last name. added a .js file. put css folder at same level as images folder. made pdf of technical spec, as of today for this assignment SBA 308.

---
+++ Band_Craft/js/bc.js
@@ -0,0 +1,9 @@
+<script>
+
+    const button = document.getElementById('submitButton');
+    button.addEventListener('onmousedown', function() {
+        this.style.backgroundColor('grey')         // Change to your desired color
+    
+    });
+
+</script>
